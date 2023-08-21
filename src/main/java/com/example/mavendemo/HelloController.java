002c6592fc6f6cfe2fd051906a17c57411218c81
sreekardevops <142288747+sreekardevops@users.noreply.github.com>
2023-08-21 14:27:36 +0530

    added

---
+++ src/main/java/com/example/mavendemo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.mavendemo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Python Life DevOPS Class Today - Aug 19 2023 - Version 10000000 !";
+    }
+
+}
